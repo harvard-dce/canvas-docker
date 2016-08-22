9ff3260fe114997c452e5a82baadcb45dd381785
Paul Gray <pgray@learningobjects.com>
2016-08-21 23:09:58 -0400

    Removing auto-generated api key since it was causing an error

--- assets/dbinit.sh
+++ assets/dbinit.sh
@@ -15,7 +15,8 @@ export CANVAS_LMS_STATS_COLLECTION="opt_out"
 cd /opt/canvas/canvas-lms \
     && bundle exec rake db:initial_setup
 
-psql -U canvas -d canvas_$RAILS_ENV -c "INSERT INTO developer_keys (api_key, email, name, redirect_uri, tool_id) VALUES ('test_developer_key', 'canvas@example.edu', 'DCE Course Admin', 'http://localhost:8000', 'canvas-docker');"
+#Todo: this might be useful to keep later
+#psql -U canvas -d canvas_$RAILS_ENV -c "INSERT INTO developer_keys (api_key, email, name, redirect_uri, tool_id) VALUES ('test_developer_key', 'canvas@example.edu', 'DCE Course Admin', 'http://localhost:8000', 'canvas-docker');"
 
 # 'crypted_token' value is hmac sha1 of 'canvas-docker' using default config/security.yml encryption_key value as secret
 psql -U canvas -d canvas_$RAILS_ENV -c "INSERT INTO access_tokens (created_at, crypted_token, developer_key_id, purpose, token_hint, updated_at, user_id) VALUES ('2015-07-01 19:33:37.596812', '4bb5b288bb301d3d4a691ebff686fc67ad49daa8', 1, 'canvas-docker', '', '2015-07-01 19:33:37.596812', 1);"
