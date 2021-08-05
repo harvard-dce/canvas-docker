01f5288bbf159e3787fbb4d08303218fb3ae3956
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2021-08-05 13:51:08 -0400

    fix db init for new contraint

--- assets/dbinit.sh
+++ assets/dbinit.sh
@@ -20,4 +20,4 @@ psql -U canvas -d canvas_development -c "INSERT INTO developer_keys (api_key, em
 # 'crypted_token' value is hmac sha1 of 'canvas-docker' using default config/security.yml encryption_key value as secret
 psql -U canvas -d canvas_development -c "INSERT INTO access_tokens (created_at, crypted_token, developer_key_id, purpose, token_hint, updated_at, user_id, root_account_id) SELECT now(), '4bb5b288bb301d3d4a691ebff686fc67ad49daa8', dk.id, 'canvas-docker', 'canva', now(), 1, 1 FROM developer_keys dk where dk.email = 'canvas@example.edu';"
 
-psql -U canvas -d canvas_development -c "INSERT INTO developer_key_account_bindings (account_id, developer_key_id, workflow_state, created_at, root_account_id) SELECT 1, dk.id, 'on', now(), 1 FROM developer_keys dk where dk.email = 'canvas@example.edu';"
+psql -U canvas -d canvas_development -c "INSERT INTO developer_key_account_bindings (account_id, developer_key_id, workflow_state, created_at, updated_at, root_account_id) SELECT 1, dk.id, 'on', now(), now(), 1 FROM developer_keys dk where dk.email = 'canvas@example.edu';"
